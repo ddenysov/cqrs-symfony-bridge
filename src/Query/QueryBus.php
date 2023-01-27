1be6d56df5f5485ef460beeac2a50244eec03139
Dmytro Denysov <dmytro.denysov@danads.se>
2023-01-27 15:31:41 +0200

    Refactoring directory structure

--- src/Query/QueryBus.php
+++ src/Query/QueryBus.php
@@ -5,12 +5,28 @@ namespace Ddenysov\CQRS\Bridge\Symfony\Query;
 use Ddenysov\CQRS\Query\Query;
 use Ddenysov\CQRS\Query\QueryBus as QueryBusInterface;
 use Ddenysov\CQRS\Query\QueryResult;
+use Ddenysov\CQRS\Query\QueryResultSingle;
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class QueryBus implements QueryBusInterface
 {
+    use HandleTrait;
+
+    /**
+     * @param MessageBusInterface $messageBus
+     */
+    public function __construct(private readonly MessageBusInterface $messageBus)
+    {
+    }
+
+    /**
+     * @param Query $query
+     * @return QueryResult
+     */
     public function send(Query $query): QueryResult
     {
-        // TODO: Implement execute() method.
+        return new QueryResultSingle($this->handle($query));
     }
 
 }
